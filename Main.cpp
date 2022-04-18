e7fa7b785784204ece0474fc970067f876c70b81
JTEH8 <jeffteh22@gmail.com>
2022-04-18 09:28:51 -0700

    Starting BST

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+#include "Tree.h"
+
+using namespace std;
+
+bool search(Tree* head);
+void printTree(Tree* head, int space);
+void treeAdd();
+
+
+int main(){
+char input[100];
+bool running = true;
+
+}
